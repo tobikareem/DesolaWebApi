eeb7a0374d8e36672f05bab94190fb55d2c707b6
tobikareem <toboibo@yahoo.com>
2024-05-13 12:23:49 -0700

    Add Environment infomation

--- Desola.Functions.Endpoints/Program.cs
+++ Desola.Functions.Endpoints/Program.cs
@@ -1,3 +1,4 @@
+using DesolaDomain.Model;
 using DesolaInfrastructure;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.DependencyInjection;
@@ -8,6 +9,19 @@ var host = new HostBuilder()
     .ConfigureServices((context, services) =>
     {
         var configuration = context.Configuration;
+
+#if DEBUG
+        var msiEnvironment = new MsiEnvironment
+        {
+            ClientSecret = configuration["Azure_Client_Secret"],
+            ClientId = configuration["Azure_Client_Id"],
+            TenantId = configuration["Azure_Tenant_Id"]
+        };
+
+        Environment.SetEnvironmentVariable("AZURE_CLIENT_ID", msiEnvironment.ClientId);
+        Environment.SetEnvironmentVariable("AZURE_TENANT_ID", msiEnvironment.TenantId);
+        Environment.SetEnvironmentVariable("AZURE_CLIENT_SECRET", msiEnvironment.ClientSecret);
+#endif
 
         services.AddMemoryCache();
         services.AddApplicationInsightsTelemetryWorkerService();
---
+++ DesolaDomain/Model/MsiEnvironment.cs
@@ -0,0 +1,8 @@
+﻿namespace DesolaDomain.Model;
+
+public class MsiEnvironment
+{
+    public string? TenantId { get; set; } = default!;
+    public string? ClientId { get; set; } = default!;
+    public string? ClientSecret { get; set; } = default!;
+}
