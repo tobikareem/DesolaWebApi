98379923a49fc15b6b7f4da96774ec990dfc60fe
tobikareem <toboibo@yahoo.com>
2025-02-16 16:15:59 -0800

    Add update service

---
+++ DesolaServices/Handler/UpdateWebSectionCommandHandler.cs
@@ -0,0 +1,20 @@
+﻿using DesolaDomain.Entities.PageEntity;
+using DesolaServices.Commands.Requests;
+using DesolaServices.Interfaces;
+using MediatR;
+
+namespace DesolaServices.Handler;
+
+public class UpdateWebSectionCommandHandler : IRequestHandler<UpdateWebSectionCommand>
+{
+    private readonly ITableBase<WebSection> _table;
+    public UpdateWebSectionCommandHandler(ITableBase<WebSection> table)
+    {
+        _table = table;
+    }
+
+    public async Task Handle(UpdateWebSectionCommand request, CancellationToken cancellationToken)
+    {
+        await _table.UpdateTableEntityAsync(request.WebSection);
+    }
+}
